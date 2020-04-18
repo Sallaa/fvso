61f2222b7e05db0a93a8300cfb87edac799da717
irisval <iris.val19@gmail.com>
2020-04-18 16:08:59 -0400

    Added article search.

--- app/controllers.py
+++ app/controllers.py
@@ -1,2 +1,16 @@
-from app import app
+from app import CONFIG, app
 from .models import *
+from newspaper import Article
+from newsapi import NewsApiClient
+
+news_api = NewsApiClient(CONFIG["NEWS_API_KEY"])
+
+def parse_url(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    # article.nlp()
+    data = {"title": article.title, "author": article.authors, "date": article.publish_date, "img": article.top_image, "tags": article.keywords}
+
+    return data
+
--- app/views.py
+++ app/views.py
@@ -1,8 +1,25 @@
-from flask import render_template
+from flask import render_template, request, flash, redirect, url_for
+from urllib.parse import urlparse
 from . import controllers as controller
 from app import app
+from .forms import SearchForm
+from validator_collection import checkers, errors
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+    return render_template('index.html', form=SearchForm())
 
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        query = request.form['search']
+
+        if checkers.is_url(query):
+            data = controller.parse_url(query)
+            return render_template('analysis.html', url=query, data=data)
+        else:
+            flash('not url')
+            return redirect('/')
+    return redirect('/')
